caf18ebc537304be88359ba1805e4e1330416ba8
camilo23401 <ca.garciav@uniandes.edu.co>
2020-08-24 09:38:32 -0500

    Reconocimiento de texto en imágenes

---
+++ muestraTecnología/main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Camilo García\Desktop\MuestraTecnologiaPMC-8a0538d9e753.json"
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                     for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+detect_text(r"C:\Users\Camilo García\Pictures\WhatsApp Image 2020-08-24 at 9.14.21 AM.jpeg")
